edffb0b93eec5fee9be5ed1d55b1a4befbf2f91d
Mathias Galleto <mathias.galleto@anima.edu.uy>
2018-07-22 20:19:40 -0300

    Se termina lo principal

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -18,9 +18,108 @@ app.controller('home', function($scope) {
             url: 'pageFive'
         }
     };
-    $scope.algoMas = "<h3>Lim</h3>√(<input type='text' id='a'>x²+<input type='text' id='b'>)<h3>x→</h3><input type='text' id='x' placeholder='a'><button id='button'>Dale al botón</button>";
     $scope.dir = 'funcionSimple.html';
     $scope.algo = function(url) {
         $scope.view=url;
     };
+
+    $scope.operate = function(a, b, x, c){
+        a = a.aValue;
+        b = b.bValue;
+        x = x.xValue;
+
+        if ($scope.view == 'pageThree') {
+            c = c.cValue;
+        }
+        else {
+            c = "1";
+        }
+
+        var funcionValdada = (validar(a, b, c, x));
+
+        if (funcionValdada) {
+            var resultado = (ordenar(a, b, c, x, $scope.view));
+            $("#mensaje").text(resultado);
+        }else {
+            $("#mensaje").text("Debe ingresar valoers válidos para continuar");
+        }
+    }
+
+    function validar(a, b, c, x){
+    
+        a = a.trim();
+        b = b.trim();
+        c = c.trim();
+        x = x.trim();
+    
+        return (isNumber(a)&&isNumber(x)&&isNumber(b)&&isNumber(c));
+    }
+    
+    function ordenar(a, b, c, x, page){
+        switch (page) {
+            case "pageTwo":
+                return (funcionSimple(a, b, x));
+                break;
+            case "pageThree":
+                return (FuncionConDivicion(a, b, c, x));
+                break;
+            case "pageFour":
+                return funcionConRaiz(a, b, x);
+                break;
+            case "pageFive":
+                return funcionConRaizDos(a, b, x);
+                break;
+            default:
+                return "Error";
+                break;
+        }
+    }
+
+        function funcionSimple(a, b, x){
+            var resultado = parseFloat(a)*parseFloat(x);
+            var resultado = parseFloat(resultado) + parseFloat(b)
+            return resultado;
+        }
+
+        function FuncionConDivicion(a, b, c, x){
+            var divicion = parseFloat(x)+parseFloat(c);
+            if (divicion == 0){
+                return "No hay limite en este punto";
+            }
+            else {
+                var resultado = parseFloat(a)*parseFloat(x);
+                var resultado = parseFloat(resultado) + parseFloat(b);
+                var resultado = parseFloat(resultado) / parseFloat(divicion);
+                return resultado;
+            }
+        }
+
+        function funcionConRaiz(a, b, x){
+            var resultado = parseFloat(a)* parseFloat(x);
+            var resultado = parseFloat(resultado) + parseFloat(b)
+            if (resultado < parseFloat(0)) {
+                return "No hay limite en este punto";
+            }
+            else {
+                var resultado = Math.sqrt(resultado);
+                return resultado;
+            }
+        }
+        
+        function funcionConRaizDos(a, b, x){
+            var resultado = parseFloat(a)* parseFloat(Math.pow(x, 2));
+            var resultado = parseFloat(resultado) + parseFloat(b)
+            if (resultado < parseFloat(0)) {
+                return "No hay limite en este punto";
+            }
+            else {
+                var resultado = Math.sqrt(resultado);
+                return resultado;
+            }
+        }
+        function isNumber(n) {
+            return !isNaN(parseFloat(n)) && isFinite(n);
+        }
+
 });
+
